7407086e1d7f9e08f5f7d5010340a5f568bc426f
JoseVitorAgatte <jvoaht@gmail.com>
2025-05-16 19:47:49 -0300

    tratativa erro login e senha incorretos

--- src/main/java/com/fiap/g10/g10auth/controller/handler/ExceptionHandlerController.java
+++ src/main/java/com/fiap/g10/g10auth/controller/handler/ExceptionHandlerController.java
@@ -7,6 +7,8 @@ import com.fiap.g10.g10auth.exception.DadoNaoEncontradoException;
 import com.fiap.g10.g10auth.exception.SenhaIncorretaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +33,11 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(SenhaIncorretaException.class)
     public ResponseEntity<MensagemErroDTO> tratarSenhaIncorreta(SenhaIncorretaException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MensagemErroDTO(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<MensagemErroDTO> trataLoginSenhaIncorreto(BadCredentialsException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MensagemErroDTO(ex.getMessage()));
     }
 
