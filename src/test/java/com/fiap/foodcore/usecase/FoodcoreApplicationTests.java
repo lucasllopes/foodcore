1d946c8d637b2297e5f5f1410a4ef7577283e4fb
Lucas Lopes <lucas.llopes99@gmail.com>
2025-07-21 11:06:46 -0300

    remocao de testes que nao estao sendo executados

--- src/test/java/com/fiap/foodcore/usecase/FoodcoreApplicationTests.java
+++ src/test/java/com/fiap/foodcore/usecase/FoodcoreApplicationTests.java
@@ -1,13 +1,7 @@
 package com.fiap.foodcore.usecase;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class FoodcoreApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
-
 }
