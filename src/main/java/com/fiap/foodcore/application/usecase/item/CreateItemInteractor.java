00dd328eea20b9a8e29a03d14a1f20cc68347747
carlospaulinodevjava <carlospaulinodevjava@gmail.com>
2025-08-02 20:22:12 -0300

    Cardapios e Items ajustes após testes.

--- src/main/java/com/fiap/foodcore/application/usecase/item/CreateItemInteractor.java
+++ src/main/java/com/fiap/foodcore/application/usecase/item/CreateItemInteractor.java
@@ -1,19 +1,27 @@
 package com.fiap.foodcore.application.usecase.item;
 
+import com.fiap.foodcore.application.exception.DuplicatedDataException;
+import com.fiap.foodcore.application.gateway.ItemGateway;
 import com.fiap.foodcore.application.usecase.input.CreateItemInput;
 import com.fiap.foodcore.application.usecase.output.ItemCreateOutput;
 import com.fiap.foodcore.domain.Item;
-import com.fiap.foodcore.infrastructure.gateways.ItemRepositoryGateway;
 
 public class CreateItemInteractor {
 
-    private final ItemRepositoryGateway itemRepositoryGateway;
 
-    public CreateItemInteractor(ItemRepositoryGateway itemRepositoryGateway) {
-        this.itemRepositoryGateway = itemRepositoryGateway;
+    private final ItemGateway itemGateway;
+
+    public CreateItemInteractor(ItemGateway itemGateway) {
+        this.itemGateway = itemGateway;
     }
 
     public ItemCreateOutput execute(CreateItemInput createItemInput) {
+
+        itemGateway.findByName(createItemInput.name())
+                .ifPresent(i -> {
+                    throw new DuplicatedDataException("Nome item já em uso");
+                });
+
         var item = new Item.Builder()
                 .name(createItemInput.name())
                 .description(createItemInput.description())
@@ -22,7 +30,7 @@ public class CreateItemInteractor {
                 .photo(createItemInput.photo())
                 .build();
 
-        itemRepositoryGateway.save(item);
+        itemGateway.save(item);
 
         return new ItemCreateOutput(
                 item.getId(),
--- src/main/java/com/fiap/foodcore/infrastructure/gateways/persistence/ItemRepository.java
+++ src/main/java/com/fiap/foodcore/infrastructure/gateways/persistence/ItemRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
     Optional<ItemEntity> findByName(String name);
     List<ItemEntity> findByMenus_Id(Long menuId);
+    List<ItemEntity> findByNameIgnoreCase(String name);
 }
